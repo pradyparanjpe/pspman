dd16dee005adaa75cba3e83e1b98d3c9e969f4e3
pradyparanjpe <pradyparanjpe@rediffmail.com>
2020-09-25 21:24:28 +0530

    bump pypi dev2 ver

--- setup.py
+++ setup.py
@@ -80,7 +80,7 @@ class PostInstallCommand(install):
 
 setup(
     name='pspman',
-    version='0.0.0.0dev1',
+    version='0.0.0.0dev2',
     description="Personal Simple Package Manager",
     license="GPLv3",
     long_description=LONG_DESCRIPTION,
