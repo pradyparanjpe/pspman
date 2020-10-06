ca8cf294b6edcdaff15b94dd0bb00cdc67984e89
pradyparanjpe <pradyparanjpe@rediffmail.com>
2020-10-06 19:19:12 +0530

    LGPL, added psprint dependency

--- pspman/gitman.py
+++ pspman/gitman.py
@@ -5,16 +5,16 @@
 # This le is part of pspman.
 #
 # pspman is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
+# it under the terms of the GNU Lesser General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
 # pspman is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# GNU Lesser General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
+# You should have received a copy of the GNU Lesser General Public License
 # along with pspman.  If not, see <https://www.gnu.org/licenses/>.
 #
 '''
